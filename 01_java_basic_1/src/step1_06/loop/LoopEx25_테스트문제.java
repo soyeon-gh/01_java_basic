2d263a57764d8da65b4bcfe44f60dfb8aaabd539
19시_웹개발_ksy <19__ksy@DESKTOP-F0Q81EV>
2021-02-16 21:34:17 +0900

    commit

--- 01_java_basic_1/src/step1_06/loop/LoopEx25_테스트문제.java
+++ 01_java_basic_1/src/step1_06/loop/LoopEx25_테스트문제.java
@@ -24,21 +24,48 @@ public class LoopEx25_테스트문제 {
 	public static void main(String[] args) {
 
 		System.out.println("문제1) 9의 배수중 십의 자리가 6인 첫번째 배수 출력");
+		System.out.println();
 
 		for (int i = 1; i < 100; i++) {
 			int answer = i * 9;
 			int number10 = answer / 10;
-			
-			
-		}
-		
 
-		System.out.println("문제2) 8로 나누면 5가 남는 수 중");
-		System.out.println("       150보다 적은 수 중에 가장 가까운수를 구하시요");
-		System.out.println("       ( 절대값 사용 x )");
+			if (number10 == 6) {
+				System.out.println("답 : " + answer);
+			}
+
+		}
+		System.out.println();
+
+		System.out.println("문제2) 8로 나누면 5가 남는 수 중 150보다 적은 수 중에");
+		System.out.println("       가장 가까운수를 구하시요( 절대값 사용 x )");
+		System.out.println();
+
+		int answer = 0;
+		for (int i = 1; i < 150; i++) {
+			int rest = i % 8;
+			if (rest == 5) {
+				answer = i;
+			}
+		}
+		System.out.println("답 : " + answer);
+		System.out.println();
 
 		System.out.println("문제3) 50에서 100까지 자연수중에서 9의 배수는 모두 몇개입니까?");
+		System.out.println();
 
+		int cnt = 0;
+		int drainage = 0;
+		
+		for (int i = 50; i <= 100; i++) {
+			if(i / 9 == 0) {
+				cnt ++;
+				drainage = i;
+				
+			}
+		}
+		System.out.println("답 : " + cnt + "개" + drainage);
+		System.out.println();
 		System.out.println("문제4) 28의 배수 중에서 가장 큰 세자리 수를 구하시요.");
 
 		System.out.println("문제5) 8의 배수를 작은수부터 5개 출력");
